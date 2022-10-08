4f1b3619556348925beaf89cded9134142f75ee8
nixon <jeromenixon95@gmail.com>
2022-10-07 21:26:17 -0700

    added sample readme

--- index.js
+++ index.js
@@ -28,58 +28,58 @@ inquirer
     {
       type: "confirm",
       name: "confirmingImage",
-      message: "Would you like to include a image?"
+      message: "Would you like to include a image?",
     },
     {
-    type: "input",
-     name: "imageSrc",
-     message: "Enter the image path:",
-     when: (answers) => answers.confirmingImage
+      type: "input",
+      name: "imageSrc",
+      message: "Enter the image path:",
+      when: (answers) => answers.confirmingImage,
     },
     {
       type: "confirm",
       name: "secondImage",
-      message: "Would you like to include a second image?"
+      message: "Would you like to include a second image?",
     },
     {
-    type: "input",
-     name: "imageSrc2",
-     message: "Enter the image path:",
-     when: (answers) => answers.secondImage
+      type: "input",
+      name: "imageSrc2",
+      message: "Enter the image path:",
+      when: (answers) => answers.secondImage,
     },
     {
       type: "confirm",
       name: "confirmingVideo",
-      message: "Would you like to include a video?"
+      message: "Would you like to include a video?",
     },
     {
-    type: "input",
-     name: "videoSrc",
-     message: "Enter the image path:",
-     when: (answers) => answers.confirmingVideo
+      type: "input",
+      name: "videoSrc",
+      message: "Enter the image path:",
+      when: (answers) => answers.confirmingVideo,
     },
     {
       type: "confirm",
       name: "confirmLicense",
-      message: "Are any licenses needed?"
+      message: "Are any licenses needed?",
     },
     {
       type: "list",
       name: "license",
       message: "what license is needed?",
       choices: ["MIT", "Apache"],
-      when: (answers) => answers.confirmLicense
+      when: (answers) => answers.confirmLicense,
     },
     {
       type: "confirm",
       name: "confirmFeature",
-      message: "Was there any features?"
+      message: "Was there any features?",
     },
     {
       type: "input",
       name: "features",
       message: "Please list features:",
-      when: (answers) => answers.confirmFeature
+      when: (answers) => answers.confirmFeature,
     },
     {
       type: "input",
@@ -94,20 +94,19 @@ inquirer
     {
       type: "confirm",
       name: "confirmingtest",
-      message: "Would you like to add testing instructions?"
+      message: "Would you like to add testing instructions?",
     },
     {
       type: "input",
       name: "test",
       message: "Please supply the testing instructions:",
-      when: (answers) => answers.confirmingtest
+      when: (answers) => answers.confirmingtest,
     },
     {
       type: "input",
       name: "github",
       message: "What is your Github username?",
     },
-    
   ])
 
   .then((answers) => {
---
+++ sampleREADME.md
@@ -0,0 +1,49 @@
+
+# Project Dogs
+
+## License
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+[License: MIT](https://opensource.org/licenses/MIT)
+	
+
+## Table of contents
+* [Title](#title) 
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribute](#contribute)
+* [Questions](#questions)
+* [Github](#github)
+
+## Description
+This is a project that is used to generate and share photos of dogs. 
+    
+## Installation
+This application is installed by downloading node.js, downloading inquirer 8.2.4, as well as dotenv 16.0.3
+
+## Usage
+You use this application however you like. 
+
+![image](assets\puppy.jpg)
+
+![image](assets\moredogs.jpg)
+
+
+    
+## 
+
+
+## How to contribute
+You can contribute by sharing more photos of dogs or improving the product by cloning or forking from Github
+
+## 
+
+
+## Questions
+For any questions regarding this applications, such as how to use it or how to contribute. You may contact through this Github account.
+
+contact Info:
+Github: https://github.com/Jeromemn
+Email: [Jeromenixon95@gmail.com](mailto:Jeromenixon95@gmail.com)
